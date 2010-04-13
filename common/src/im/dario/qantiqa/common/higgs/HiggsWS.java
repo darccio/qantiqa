975b0ef931beb6c28a5f5f4ec6e3f7f75caa8eef
are.belong.to.dario <are.belong.to.dario@86696dec-2a0f-11df-b301-8d15aaa0c3c2>
2010-04-13 19:37:46 +0000

    Improved docs and licensing

--- common/src/im/dario/qantiqa/common/higgs/HiggsWS.java
+++ common/src/im/dario/qantiqa/common/higgs/HiggsWS.java
@@ -1,21 +1,57 @@
+/*******************************************************************************
+ * Qantiqa : Decentralized microblogging platform
+ * Copyright (C) 2010 Dario (i@dario.im) 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+
 package im.dario.qantiqa.common.higgs;
 
 import im.dario.qantiqa.common.protocol.Protocol;
 import im.dario.qantiqa.common.protocol.format.XmlFormat;
 
-import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 import play.Play;
 import play.libs.WS.HttpResponse;
 
-import com.google.protobuf.XmlFormat.ParseException;
+import com.google.protobuf.Message.Builder;
 
+/**
+ * Higgs webservice client.
+ * 
+ * Allows to interact with Higgs public API for gluons. It depends on Play
+ * because our webapps depend too.
+ * 
+ * @author Dario
+ */
 public class HiggsWS {
 
-    public static Protocol.validation validate(Integer port, String secret)
-            throws ParseException {
+    /**
+     * Validates a gluon against our official list.
+     * 
+     * @see {@link HiggsWS#gluons()}
+     * 
+     * @param port
+     *            Port published by the gluon.
+     * @param secret
+     *            Secret used to validate the gluon against the list.
+     * @return Validation result message
+     */
+    public static Protocol.validation validate(Integer port, String secret) {
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("secret", play.libs.Codec.hexMD5(secret));
         params.put("port", port);
@@ -23,23 +59,41 @@ public class HiggsWS {
         HttpResponse rs = play.libs.WS.url(getHiggsURL() + "/validate").params(
                 params).post();
 
-        Protocol.validation.Builder builder = Protocol.validation.newBuilder();
-        XmlFormat.merge(new ByteArrayInputStream(rs.getString().getBytes()),
-                builder);
-
-        return builder.build();
+        return getMessageFromXML(rs, Protocol.validation.newBuilder()).build();
     }
 
-    public static Protocol.gluons gluons() throws ParseException {
+    /**
+     * Get the official gluon (supernode) list from Higgs.
+     * 
+     * @return Gluon list message
+     */
+    public static Protocol.gluons gluons() {
         HttpResponse rs = play.libs.WS.url(getHiggsURL() + "/gluons").get();
 
-        Protocol.gluons.Builder builder = Protocol.gluons.newBuilder();
-        XmlFormat.merge(new ByteArrayInputStream(rs.getString().getBytes()),
-                builder);
-
-        return builder.build();
+        return getMessageFromXML(rs, Protocol.gluons.newBuilder()).build();
     }
 
+    /**
+     * Auxiliary method to retrieve Protobuf messages from an HTTP XML stream.
+     * 
+     * @param rs
+     *            HTTP request response
+     * @param builder
+     *            Protobuf message builder
+     * @return Builder after merging with XML stream.
+     */
+    private static <V extends Builder> V getMessageFromXML(HttpResponse rs,
+            V builder) {
+        XmlFormat.merge(rs.getStream(), builder);
+
+        return builder;
+    }
+
+    /**
+     * Auxiliary method to get the Higgs URL from Play configuration.
+     * 
+     * @return Higgs webservice URL
+     */
     private static String getHiggsURL() {
         return Play.configuration.getProperty("qantiqa.higgs.url");
     }
---
+++ gluon-mock/src/im/dario/qantiqa/mock/gluon/GluonMock.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Qantiqa : Decentralized microblogging platform
+ * Copyright (C) 2010 Dario (i@dario.im) 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+
+package im.dario.qantiqa.mock.gluon;
+
+import network.Overlay;
+
+/**
+ * Auxiliary class intended to simulate/mock a gluon, without webservice
+ * interaction nor qa-api-node initialization.
+ * 
+ * It will help to a faster debugging, without requiring an active webservice
+ * due gluons are not used as normal peers to send quarks.
+ * 
+ * Anyway, all the code and conf are developed inside qa-node-api project
+ * classes, making this a lightweight class.
+ * 
+ * @author Dario
+ */
+public class GluonMock {
+
+    public void start() {
+        // Path relative to $workspace/qantiqa/gluon-mock/
+        String cfgPath = "../api/conf/easypastry-config.xml";
+
+        Overlay overlay = Overlay.initGluon(cfgPath);
+        overlay.bootGluon();
+    }
+
+    public static void main(String[] args) {
+        GluonMock app = new GluonMock();
+        app.start();
+    }
+}
