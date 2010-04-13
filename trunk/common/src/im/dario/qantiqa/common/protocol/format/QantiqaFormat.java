3121e4d059fd03a4be017f0faf8aa97526f8dce5
are.belong.to.dario <are.belong.to.dario@86696dec-2a0f-11df-b301-8d15aaa0c3c2>
2010-04-13 20:45:41 +0000

    Higgs documented and interoperable format improved

---
+++ trunk/common/src/im/dario/qantiqa/common/protocol/format/QantiqaFormat.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Qantiqa : Decentralized microblogging platform
+ * Copyright (C) 2010 Dario (i@dario.im) 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+
+package im.dario.qantiqa.common.protocol.format;
+
+import java.io.InputStream;
+
+import com.google.protobuf.Message;
+import com.google.protobuf.Message.Builder;
+
+/**
+ * Wrapper class around {@link XmlFormat} to separate the specific format
+ * serializer to ease future changes.
+ * 
+ * @author Dario
+ */
+public class QantiqaFormat {
+
+    public static void merge(InputStream input, Builder builder) {
+        XmlFormat.merge(input, builder);
+    }
+
+    public static String printToString(Message message) {
+        return XmlFormat.printToString(message);
+    }
+}
--- trunk/higgs/app/models/Gluon.java
+++ trunk/higgs/app/models/Gluon.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Qantiqa : Decentralized microblogging platform
+ * Copyright (C) 2010 Dario (i@dario.im) 
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+
 package models;
 
 import java.util.List;
@@ -11,6 +30,13 @@ import siena.NotNull;
 import siena.Query;
 import siena.Table;
 
+/**
+ * Gluon model class.
+ * 
+ * Used by Siena to persist our gluons data in the GAE Datastore.
+ * 
+ * @author Dario
+ */
 @Table("gluon")
 public class Gluon extends Model {
 
@@ -21,6 +47,7 @@ public class Gluon extends Model {
     @NotNull
     public String host;
 
+    @Index("ix_host")
     @NotNull
     public Integer port;
 
@@ -37,18 +64,40 @@ public class Gluon extends Model {
         this.active = Boolean.TRUE;
     }
 
+    /**
+     * 
+     * @return All gluons..
+     */
     public static Query<Gluon> all() {
         return Model.all(Gluon.class);
     }
 
+    /**
+     * 
+     * @param id
+     *            Gluon's id
+     * @return Matching gluon with provided id.
+     */
     public static Gluon findById(Long id) {
         return all().filter("id", id).get();
     }
 
+    /**
+     * 
+     * @param host
+     *            Gluon's WAN IP
+     * @param port
+     *            Gluon's published port
+     * @return Matching gluon.
+     */
     public static Gluon findByEndpoint(String host, Integer port) {
         return all().filter("host", host).filter("port", port).get();
     }
 
+    /**
+     * 
+     * @return Only active gluons.
+     */
     public static List<Gluon> active() {
         return all().filter("active", Boolean.TRUE).fetch();
     }
